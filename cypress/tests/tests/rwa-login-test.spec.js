a785be35247145f510697c652715f9498065ebac
Nehakee <mathematyk@gmail.com>
2022-06-08 21:17:49 +0200

    {code correct}

--- cypress/tests/tests/rwa-login-test.spec.js
+++ cypress/tests/tests/rwa-login-test.spec.js
@@ -6,15 +6,26 @@ describe("Main test Real world app", () => {
   before(() => {
     cy.visit("http://localhost:3000");
   });
+  it("Register account", () => {
+    cy.get('[data-test="signup"]').click();
+    cy.url().should("eq", "http://localhost:3000/signup");
+    cy.get('[id="firstName"]').should("be.visible").type("Abel");
+    cy.get('[id="lastName"]').should("be.visible").type("Fernandez");
+    cy.get('[id="username"]').should("be.visible").type("Abel1");
+    cy.get('[id="password"]').should("be.visible").type("abel1");
+    cy.get('[id="confirmPassword"]').should("be.visible").type("abel1");
+    cy.get('[class="MuiButton-label"]').should("be.visible").click();
+    cy.url().should("eq", "http://localhost:3000/signin");
+  });
 
   it("Checkout login page", () => {
-    cy.get('[class="makeStyles-logo-3"]').should("be.visible");
+    cy.get('[class="makeStyles-logo-22"]').should("be.visible");
     cy.get('[class="MuiTypography-root MuiTypography-h5"]')
       .should("exist")
       .and("contain", "Sign in");
     cy.get('[id="username"]').should("be.visible");
     cy.get('[id="password"]').should("be.visible");
-    cy.get('[class="PrivateSwitchBase-input-14"]').should("not.be.checked");
+    cy.get('[class="PrivateSwitchBase-input-33"]').should("not.be.checked");
     cy.get('[class="MuiButton-label"]').should("be.visible");
     cy.get('[data-test="signup"]').should("be.visible").click();
     cy.url().should("eq", "http://localhost:3000/signup");

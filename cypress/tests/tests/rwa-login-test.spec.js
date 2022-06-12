c9698de9167385f8775fb61e75073b3d48505a1b
Nehakee <mathematyk@gmail.com>
2022-06-12 17:29:09 +0200

    CR fixes

--- cypress/tests/tests/rwa-login-test.spec.js
+++ cypress/tests/tests/rwa-login-test.spec.js
@@ -1,6 +1,5 @@
 /// <reference types="cypress" />
-const username = "Abel1";
-const password = "abel1";
+const Registerdata = ["Abel", "Fernandez", "Abel1", "abel1"];
 
 describe("Main test Real world app", () => {
   before(() => {
@@ -9,11 +8,11 @@ describe("Main test Real world app", () => {
   it("Register account", () => {
     cy.get('[data-test="signup"]').click();
     cy.url().should("eq", "http://localhost:3000/signup");
-    cy.get('[id="firstName"]').should("be.visible").type("Abel");
-    cy.get('[id="lastName"]').should("be.visible").type("Fernandez");
-    cy.get('[id="username"]').should("be.visible").type("Abel1");
-    cy.get('[id="password"]').should("be.visible").type("abel1");
-    cy.get('[id="confirmPassword"]').should("be.visible").type("abel1");
+    cy.get('[id="firstName"]').should("be.visible").type(Registerdata[0]);
+    cy.get('[id="lastName"]').should("be.visible").type(Registerdata[1]);
+    cy.get('[id="username"]').should("be.visible").type(Registerdata[2]);
+    cy.get('[id="password"]').should("be.visible").type(Registerdata[3]);
+    cy.get('[id="confirmPassword"]').should("be.visible").type(Registerdata[3]);
     cy.get('[class="MuiButton-label"]').should("be.visible").click();
     cy.url().should("eq", "http://localhost:3000/signin");
   });
@@ -33,21 +32,19 @@ describe("Main test Real world app", () => {
 
   it("Website login check", () => {
     cy.go("back");
-    //cy.visit('http://localhost:3000')
     cy.url().should("eq", "http://localhost:3000/signin");
     cy.wait(1000);
     cy.intercept("POST", "http://localhost:3001/login").as("login");
-    cy.get('[id="username"]').type(username);
-    cy.get('[id="password"]').type(password);
+    cy.get('[id="username"]').type(Registerdata[2]);
+    cy.get('[id="password"]').type(Registerdata[3]);
     cy.get('[class="MuiButton-label"]').click();
     cy.url().should("eq", "http://localhost:3000/");
 
     cy.wait("@login").then((request) => {
-      expect(request.request.body.username).to.contain("Abel1");
-      expect(request.request.body.password).to.contain("abel1");
+      expect(request.request.body.username).to.contain(Registerdata[2]);
+      expect(request.request.body.password).to.contain(Registerdata[3]);
       expect(request.response.statusCode).to.eq(200);
     });
     cy.get('[role="dialog"]').should("be.visible");
   });
-  //test
 });
